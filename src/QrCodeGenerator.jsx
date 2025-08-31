a23df9a15952720559f02ca04464cbe794aa1c5c
ilyafredriksson <fredrikssonilya@gmail.com>
2025-08-31 21:10:48 +0200

    QR code komponent finish

--- src/QrCodeGenerator.jsx
+++ src/QrCodeGenerator.jsx
@@ -1,9 +1,36 @@
 import {QRCodeSVG} from 'qrcode.react';
+import{  useState } from 'react';
 
 const QrCodeGenerator = () => {
+const [value, setValue] = useState ('hello')
+const [result ,setResult] = useState ('')
+
+
+
+//console.log(array[0]);//value   
+//console.log(array[1]);//function to change value
+
+const onClickHandler = (event) => {
+   // console .log('dfdfh');
+    setResult(value);
+    setValue('')
+};
+ 
+const onChangeHandler=(event) => {
+   setValue(event.target.value);
+  setResult('');
+ 
+  console.log(result)
+}
   return (
     <div>
-        <QRCodeSVG value="tjena hur är det" />
+        
+        {result !=='' && <QRCodeSVG value={result} />}
+       
+        <input type="text" value={value} onChange={onChangeHandler}/>
+        <button type="button"onClick={onClickHandler}>
+            Generate QR
+    </button>
     </div>
   )
 }
